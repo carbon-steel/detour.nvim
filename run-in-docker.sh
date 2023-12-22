b292c65b25d70a790d8b82113163b67288830242
Roger <roger.kim486@gmail.com>
2023-12-22 12:31:50 -0500

    Fix testing

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -2,5 +2,5 @@
 # luarocks init
 # luarocks config --scope project lua_version 5.1
 nvim -u NONE \
-  -c "lua package.path='lua_modules/share/lua/5.1/?.lua;lua_modules/share/lua/5.1/?/init.lua;'..package.path;package.cpath='lua_modules/lib/lua/5.1/?.so;'..package.cpath;local k,l,_=pcall(require,'luarocks.loader') _=k and l.add_context('busted','$BUSTED_VERSION')" \
-  -l "lua_modules/lib/luarocks/rocks-5.1/busted/$BUSTED_VERSION/bin/busted" .
+  -c "lua local k,l,_=pcall(require,'luarocks.loader') _=k and l.add_context('busted','$BUSTED_VERSION')" \
+  -l "/usr/local/lib/luarocks/rocks-5.1/busted/$BUSTED_VERSION/bin/busted" .
